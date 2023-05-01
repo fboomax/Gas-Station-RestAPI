33004377e775c8ee9573bf53eb3b58d879a46545
fboomax <fboomax@gmail.com>
2023-05-01 13:34:13 +0300

    the change is completed!!

--- restApi/settings.py
+++ restApi/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import pymysql
 
 from pathlib import Path
 
+# pymysql.version_info = (1, 3, 13, "final", 0)
+
+pymysql.version_info = (1, 4, 6, 'final', 0)
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,10 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'fuelApi',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,21 +75,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restApi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "fuel",
-        "USER": "uth",
-        "PASSWORD": "cangetin",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fuel',
+        'USER': 'uth',
+        'PASSWORD': 'cangetin',
         "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+        "PORT": 3306,  # default postgres port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -104,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -115,7 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
