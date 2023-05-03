dd065accf30965d4f9f6ba8bb6a80fb1751e5ffc
fboomax <fboomax@gmail.com>
2023-05-03 16:43:57 +0300

    views.py: gas_station_all creat RESTAPI

--- fuelApi/urls.py
+++ fuelApi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import gas_station_all
+from .views import *
 
 urlpatterns = [
     path('api/v1/gas-stations', gas_station_all),
+    path('api/v1/gas-stations/<int:pk>', gas_station_detail),
+    # path('api/v1/gas-stations', gas_station_all),
 ]
--- fuelApi/views.py
+++ fuelApi/views.py
@@ -1,21 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
-from .models import *
 from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import GasStation
 
 
+@api_view(['GET', 'POST'])
 def gas_station_all(request):
+
     if request.method == 'GET':
-        gasstations = GasStation.objects.all()
-        serializer = GasStationSerializer(gasstations, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        gas_stations = GasStation.objects.all()
+        serializer = GasStationSerializer(gas_stations, many=True)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = GasStationSerializer(data=data)
+        # data = JSONParser().parse(request)
+        serializer = GasStationSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
+            # return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view
+def gas_station_detail(request, pk):
+    try:
+        gas_station = GasStation.objects.get(pk=pk)
+
+    except GasStation.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = GasStationSerializer(gas_station)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = GasStationSerializer(gas_station, data=data)
+        if serializer.is_valid():
+            serializer.save()
             return JsonResponse(serializer.data, status=201)
-        else:
-            return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        gas_station.delete()
+        return HttpResponse(status=204)
+
+
